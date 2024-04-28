f9368fefbbd91206d1cc1178cbd3a3230a681598
Anita Gulacsi <gulacsi_anita@yahoo.com>
2024-04-28 22:48:23 +0300

    Separate ImageProcessor logic into custom hook

--- src/components/image-processor/ImageProcessor.tsx
+++ src/components/image-processor/ImageProcessor.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { API_CONFIG } from "@/pages/api/apiRoutes";
+import React from "react";
+import { useLabelDetection } from "./useLabelDetection";
 
 interface ImageProcessorProps {
   imageDataUrl: string;
@@ -9,42 +8,7 @@ interface ImageProcessorProps {
 export const ImageProcessor: React.FC<ImageProcessorProps> = ({
   imageDataUrl,
 }) => {
-  const [labels, setLabels] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchLabels = async () => {
-      if (!imageDataUrl) return;
-
-      setIsLoading(true);
-      const url = `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.VISION_API}?key=${API_CONFIG.API_KEY}`;
-      const body = {
-        requests: [
-          {
-            image: {
-              content: imageDataUrl.split(",")[1], // Remove the base64 header
-            },
-            features: [{ type: "LABEL_DETECTION", maxResults: 5 }],
-          },
-        ],
-      };
-
-      try {
-        const response = await axios.post(url, body);
-        const labels = response.data.responses[0].labelAnnotations.map(
-          (label: any) => label.description
-        );
-        setLabels(labels);
-      } catch (error) {
-        console.error("Error calling Google Vision API:", error);
-        setLabels([]);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchLabels();
-  }, [imageDataUrl]);
+  const { labels, isLoading } = useLabelDetection(imageDataUrl);
 
   return (
     <div className="m-5">
